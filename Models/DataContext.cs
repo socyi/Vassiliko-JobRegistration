8d776be39f2ad45730b6c15ceb28c22e4cfd9545
socyi <63241145+socyi@users.noreply.github.com>
2021-06-22 14:48:48 +0300

    Add files via upload

---
+++ Models/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace JobsRegistration.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("name=dep_Job_Register") { }
+        public DbSet<Users> Users { get; set; }
+        public DbSet<Job_Status> Job_Status { get; set; }
+        public DbSet<Job_Register> Job_Register { get; set; }
+        public DbSet<Departments> Department { get; set; }
+        public DbSet<Job_Category> Job_Category { get; set; }
+        public DbSet<Employees> Employee { get; set; }
+        
+    }
+}
